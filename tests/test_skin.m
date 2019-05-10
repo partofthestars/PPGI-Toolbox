6179f0c0eb45db4aa091137d3f27786bd548d9e1
Christian S. Pilz <cpi@partofthestars.com>
2019-05-10 10:13:18 +0200

    skin segmentation test

---
+++ tests/test_skin.m
@@ -0,0 +1,36 @@
+%########################################################################
+%
+%	- PPGI Toolbox - 
+%   A MATLAB toolbox for Photoplethysmography Imaging (PPGI)
+%
+% Author   : Christian S. Pilz
+% Company  : The Nature of Space of Time
+% Date     : 07.05.2019
+%
+% Contact  : cpi@partofthestars.com
+% Web Page : www.partofthestars.com
+%
+% Version  : Alpha RA 1.0
+%
+%########################################################################
+%
+%	test_skin.m:
+%
+% Description:
+%
+%   test of the skin segmentation on given sample rgb data
+%
+
+clear all;
+close all;
+
+load('./../examples/data/example_data.mat');
+
+frames=size(rgb,2);
+
+for f=1:frames
+    f
+    skin_pixels{f}=skin.get(rgb{f});
+end
+
+save('./../examples/data/example_data.mat','fs','ppg','rgb','skin_pixels','-v7.3');
