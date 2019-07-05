131b766a94c3c7dbc4d230492857def309b68a56
Christian S. Pilz <cpi@partofthestars.com>
2019-07-05 09:11:14 +0200

    Update spatial_subspace_rotation.m

--- lib/algorithm/features/spatial_subspace_rotation.m
+++ lib/algorithm/features/spatial_subspace_rotation.m
@@ -22,7 +22,7 @@
 %
 % References:
 %
-%	W. Wang, S. Stuijk and G. de Haan, "A Novel Algorithm for Remote Photoplethysmography: Spatial Subspace Rotation," 
+%   W. Wang, S. Stuijk and G. de Haan, "A Novel Algorithm for Remote Photoplethysmography: Spatial Subspace Rotation," 
 %   in IEEE Transactions on Biomedical Engineering, vol. 63, no. 9, pp. 1974-1984, Sept. 2016.
 %
 
