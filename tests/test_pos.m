c200c6b80082dcaef9122a73faba49d77e49cf1a
Christian S. Pilz <cpi@partofthestars.com>
2019-07-17 14:52:23 +0200

    Add files via upload

---
+++ tests/test_pos.m
@@ -0,0 +1,47 @@
+%########################################################################
+%
+%	- PPGI Toolbox - 
+%   A MATLAB toolbox for Photoplethysmography Imaging (PPGI)
+%
+% Author   : Christian S. Pilz
+% Company  : The Nature of Space of Time
+% Date     : 07.05.2019
+%
+% Contact  : cpi@partofthestars.com
+% Web Page : www.partofthestars.com
+%
+% Version  : beta0.1
+%
+%########################################################################
+%
+%	test_pos.m:
+%
+% Description:
+%
+%   test of the Diffusion process model. the implementation is pre-compiled
+%   currently and will be updated as soon as possible. but it should give
+%   you the picture.
+%
+
+clear all;
+close all;
+
+load('./../media/data/example_data.mat');
+
+if ~exist('skin_pixels')
+    disp('error: no skin pixels available. execute test_skin.m first!');
+    return; 
+end
+
+pos=projection_orthogonal_to_skin(45);
+pulse=pos.get(skin_pixels);
+
+fs=25;
+low=0.5;
+high=2.5;
+bpf=bandpass_filter(fs,low,high);
+pulse_f = bpf.get(pulse);
+
+
+[pearson, rmse, snr, snr_var, bpm] = ground_truth_stats.get(ppg,pulse_f,fs);
+
