87112b3929f031589a8cc9b4a8650fc8e03bd8c4
Christian S. Pilz <cpi@partofthestars.com>
2019-07-19 09:25:25 +0200

    Update startup.m

--- startup.m
+++ startup.m
@@ -28,6 +28,7 @@ addpath(genpath([pathstr '/lib']));
 addpath(genpath([pathstr '/lib/algorithm']));
 addpath(genpath([pathstr '/lib/algorithm/features']))
 addpath(genpath([pathstr '/lib/algorithm/models']))
+addpath(genpath([pathstr '/lib/algorithm/models/diffusion_process']))
 addpath(genpath([pathstr '/lib/evaluation']));
 addpath(genpath([pathstr '/lib/utils']))
 clearvars pathstr;
